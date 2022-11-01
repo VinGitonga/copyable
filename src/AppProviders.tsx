dee281aad15ed1e1a1a65c39290b5c2e19e03645
Enrique Shunnar <xtealer00@gmail.com>
2022-11-01 00:02:34 -0500

    Progress with new dashboard

---
+++ src/AppProviders.tsx
@@ -0,0 +1,23 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from 'next-auth/react'
+import { StepperProvider } from 'contexts'
+import { FC, ReactNode } from 'react'
+import AppServices from './AppServices'
+
+const AppProviders: FC<{
+  children: ReactNode | ReactNode[]
+  session: any
+  theme: any
+}> = ({ children, session, theme }) => {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <StepperProvider>
+          <AppServices>{children}</AppServices>
+        </StepperProvider>
+      </ChakraProvider>
+    </SessionProvider>
+  )
+}
+
+export default AppProviders
---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import theme from 'theme/theme'
+
+import 'styles/App.css'
+import 'styles/Contact.css'
+
+import 'react-calendar/dist/Calendar.css'
+import 'styles/MiniCalendar.css'
+import Head from 'next/head'
+import { AppPropsWithLayout } from 'types/Layout'
+import AppProviders from 'AppProviders'
+
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page)
+  return (
+    <AppProviders session={session} theme={theme}>
+      <Head>
+        <title>Horizon UI Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <React.StrictMode>
+        {getLayout(<Component {...pageProps} />)}
+      </React.StrictMode>
+    </AppProviders>
+  )
+}
+
+export default MyApp
